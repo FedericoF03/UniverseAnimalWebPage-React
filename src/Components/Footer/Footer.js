e68f25d02e9a58524f89d874cc7777f7fc461083
federico.kpo@hotmail.com.ar <federicofernandeztec@gmail.com>
2022-07-22 03:41:39 -0300

    Cambios en Link

--- src/Components/Footer/Footer.js
+++ src/Components/Footer/Footer.js
@@ -24,7 +24,7 @@ const Footer = ()=>{
                     >Inicio</Link>
                     <Link 
                     className='box__links--link'
-                    
+                    onClick={window.scrollY = "0"}
                     to='/info'
                     >Conoce sobre nosotros</Link>
                     <Link 
